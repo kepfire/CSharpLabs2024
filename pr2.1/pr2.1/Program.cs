8c1ffc07bfac48b4b72e55720e3f252f0f75c9d7
kepfire <vitaliy200475@gmail.com>
2024-03-13 12:10:39 +0200

    pr2

---
+++ pr2.1/pr2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть три цілих числа:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int num3 = Convert.ToInt32(Console.ReadLine());
+        if (num1 >= 1 && num1 <= 12)
+            Console.WriteLine($"Число {num1} належить інтервалу [1, 12].");
+
+        if (num2 >= 1 && num2 <= 12)
+            Console.WriteLine($"Число {num2} належить інтервалу [1, 12].");
+
+        if (num3 >= 1 && num3 <= 12)
+            Console.WriteLine($"Число {num3} належить інтервалу [1, 12].");
+
+        Console.ReadLine();
+    }
+}
